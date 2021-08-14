80d987b08a10be49f2a1b60ecd84ff813ce71981
vk0808 <vk4765712@gmail.com>
2021-08-14 21:29:49 +0530

    feat: welcome message

---
+++ StackQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack and Queue Program");
+        }
+    }
+}
