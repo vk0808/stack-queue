c9ea122c9aa396d1c9d9a63db0524fa817beeeb9
vk0808 <vk4765712@gmail.com>
2021-08-14 22:17:11 +0530

    feat: add node class

---
+++ StackQueue/Node.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueue
+{
+    class Node
+    {
+    }
+}
---
+++ StackQueue/Stack.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackQueue
+{
+    public class Stack
+    {
+
+    }
+}
